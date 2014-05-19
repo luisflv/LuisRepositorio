551ecfb002e5f5a3c99b028327d0e003adac176a
luisflv <luisflv@gmail.com>
2014-05-19 00:15:31 -0300

    Atualizacao do repositorio

--- LuisRepositorio/Unidade_VIII/Exercicios_Complementares/Exe03_.cs
+++ LuisRepositorio/Unidade_VIII/Exercicios_Complementares/Exe03_.cs
@@ -12,7 +12,8 @@ namespace Unidade_VIII.Exercicios_Complementares
         para gerar os cartões das vagas dos moradores. Nos cartões, é necessário constar o número do 
         bloco e o número do apartamento. Nesse condômino, há três blocos numerados de 1 a 3. Emcada bloco, há 9 andares. 
         Em cada andar, há 4 apartamentos. No primeiro andar, os números dos Apartamentos são: 11, 12, 13 e 14. 
-        No segundo andar, os números dos apartamentos são: 21, 22, 23 e 24. Nos outros andares, a lógica de numeração é a mesma*/
+        No segundo andar, os números dos apartamentos são: 21, 22, 23 e 24. Nos outros andares, a lógica de numeração é
+        a mesma*/
 
 
         static void Main3(string[] args)
---
+++ LuisRepositorio/Unidade_VIII/Exercicios_Complementares/Exe05_.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_VIII.Exercicios_Complementares
+{
+    class Exe05_
+    {
+        /*5)Considere um jogo no qual o jogador lança um dado 10 vezes.
+        O jogador ganha se a somados valores obtidos nos lançamentos for ímpar. 
+        Se o número 6 for sorteado 2 vezes o jogador perde imediatamente. 
+        O número 1 deve ser desconsiderado na somatória. 
+        Implemente um programa em C# para simular a execução desse jogo*/
+
+
+        static void Main(string[] args)
+        {
+
+            Random gerador = new Random();
+
+            int cont =0;
+
+            int lancamentoDado;
+            double soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                lancamentoDado = gerador.Next(1,7);
+
+                if (lancamentoDado == 1)
+                {
+                    continue;
+                }
+
+                soma += lancamentoDado;
+                
+                if (lancamentoDado == 6)
+                {
+                    cont++;
+                }
+                if (cont >= 2)
+                {
+                    Console.WriteLine("Voce perdeu!");
+                    break;
+                }
+            }
+
+            if (soma % 2 == 1)
+            {
+                Console.WriteLine("Voce ganhou!");
+            }
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
