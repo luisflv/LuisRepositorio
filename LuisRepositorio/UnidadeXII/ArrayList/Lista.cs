7065c278cff795b3c4e82666f5ace469dbdd15f8
luisflv <luisflv@gmail.com>
2014-06-24 09:59:07 -0300

    Atualizacao do Repositorio

---
+++ LuisRepositorio/UnidadeXII/ArrayList/Lista.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadeXII
+{
+    class Lista
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList nomes = new ArrayList();
+
+            nomes.Add("Luis");
+            nomes.Add("Fernando");
+
+            foreach (string nom in nomes)
+            {
+                Console.Write(" " +nom);
+            }
+            
+            ArrayList numerosInteriros = new ArrayList();
+
+            numerosInteriros.Add(1);
+            numerosInteriros.Add(2);
+            numerosInteriros.Add(3);
+
+            foreach (int numInt in numerosInteriros)
+            {
+                Console.Write(" "+numInt);
+            }
+
+            ArrayList numerosDecimais = new ArrayList();
+
+            numerosDecimais.Add(1.5);
+            numerosDecimais.Add(2.5);
+            numerosDecimais.Add(3.5);
+
+            foreach (double numDec in numerosDecimais)
+            {
+                Console.WriteLine("\n" + numDec);
+            }
+            
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
--- LuisRepositorio/UnidadeXII/Program.cs
+++ LuisRepositorio/UnidadeXII/Program.cs
@@ -8,7 +8,7 @@ namespace UnidadeXII
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
         }
     }
