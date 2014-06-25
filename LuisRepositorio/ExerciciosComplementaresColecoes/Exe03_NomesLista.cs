030a0ee3f98a7208e738bd9fc774edb26199316f
luisflv <luisflv@gmail.com>
2014-06-25 11:49:40 -0300

    Atualizando Complementares Colecoes - 1- 3

---
+++ LuisRepositorio/ExerciciosComplementaresColecoes/Exe03_NomesLista.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ExerciciosComplementares
+{
+    class Exe03_NomesLista
+    {
+        /*3)Faça um programa que leia n nomes inserindo-os em uma lista de forma ordenada utilizando 
+        a ideia do algoritmo da inserção. No final, o programa deve mostrar todos os nomes ordenados alfabeticamente. */
+
+        static ArrayList _ArrayList = new ArrayList();
+
+        public static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Informe os nomes: (DIGITE 'fim' PARA SAIR");
+            do
+            {
+                _ArrayList.Add(nome = Console.ReadLine());
+
+            } while (nome != "fim");
+
+            _ArrayList.Sort();
+
+            foreach (var itens in _ArrayList)
+            {
+                Console.WriteLine("Os nomes ordenados sao: " + itens);
+            }
+
+            Console.ReadKey();
+        
+        }
+    }
+}    
